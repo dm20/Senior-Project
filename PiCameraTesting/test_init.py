c64e4488398fd0b7d95e73f5b3b358f97962f47b
dm20 <demcgrath2@gmail.com>
2017-10-03 13:37:43 -0700

    Pi Camera Initialization Test

---
+++ PiCameraTesting/test_init.py
@@ -0,0 +1,10 @@
+# Pi Camera initialization test
+
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()     # initialize camera
+
+camera.start_preview()  # display camera preview on monitor
+sleep(10)               
+camera.stop_preview()   # end preview
