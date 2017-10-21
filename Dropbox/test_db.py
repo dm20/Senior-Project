8c477d1ff1ba3efa039ea06c06b6bd3f1bc3bb40
dm20 <demcgrath2@gmail.com>
2017-10-20 19:58:30 -0700

    update test_db.py

--- Dropbox/test_db.py
+++ Dropbox/test_db.py
@@ -1,5 +1,6 @@
 import dropbox 
 
-dbx.users_get_current_account()
+db = dropbox.Dropbox('9Z0y_pZ_w8sAAAAAAAAAr6wdpjNlg0XW9hTL0RIdNzkWTSfFUYZB23IQKTOjkMi8')
+db.users_get_current_account()
 
 return 0
