6e74ed5d07c1bb80410ebc656bc4890934f62fd0
dm20 <demcgrath2@gmail.com>
2017-10-20 21:46:11 -0700

    small bug fixes, passed tests
    -debugged across the board

--- PiCamera/periodic_capture.py
+++ PiCamera/periodic_capture.py
@@ -1,21 +1,24 @@
 # Periodic photo capture script
-# TODO: full test
+# Rev #2
+# TODO: time stamping, button for on/off
 
 from picamera import PiCamera
 from time import sleep
 import RPi.GPIO as GPIO
+import dropbox
 
 # LED drive
-GPIO.setmode(GPIO.BCM);
-pin = 15;
-GPIO.setup(pin,GPIO.OUT);
 GPIO.setwarnings(False);
+GPIO.setmode(GPIO.BCM);
+pin = 13;
+GPIO.setup(pin,GPIO.OUT);
+GPIO.output(pin,0)
 
 # camera and timing elements
 camera = PiCamera()     # initialize camera
 numHours = 2
-secondsPerHour = 3600
-running = true          # running = button input signal indicates system on
+secondsPerHour = 20
+running = 1          # running = button input signal indicates system on
 count = 0;
 img = 'image_'
 
@@ -24,10 +27,17 @@ camera.start_preview()  # display camera preview on monitor
 sleep(5)
 camera.stop_preview()
 
+def db_upload(image):
+  upload_name = "/" + image;
+  file1 = open(image).read()
+  db = dropbox.Dropbox('ippz4jAbhKAAAAAAAAAACSgGqnfO0L2JvjAb-YJ6l7KWZqo3uGLsjSU6d6afDKse')
+  db.files_upload(file1,upload_name)
+
 # periodically capture pictures and upload to dropbox
 while running:
-  current_img = id + str(count)
-  count += 1
+  current_img = img + str(count)
+  count+=1
+  current_img = current_img + '.jpg'
   GPIO.output(pin,1) # turn on the LED
   sleep(0.2)
   camera.capture(current_img)
@@ -35,9 +45,4 @@ while running:
   GPIO.output(pin,0) # turn off the LED
   db_upload(current_img)
   sleep(numHours*secondsPerHour)               
-  
-def db_upload(image):
-  upload_name = "/" + image;
-  file1 = open(image).read()
-  db = dropbox.Dropbox('ippz4jAbhKAAAAAAAAAACSgGqnfO0L2JvjAb-YJ6l7KWZqo3uGLsjSU6d6afDKse')
-  db.files_upload(file1,upload_name)
+
