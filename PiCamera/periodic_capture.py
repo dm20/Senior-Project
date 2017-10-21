d12d6f255e4ab9a87cdad70b733166b9323aab1c
dm20 <demcgrath2@gmail.com>
2017-10-20 22:03:32 -0700

    more efficient naming of files and recordkeeping
    Now we believe the files names are being overweritten in the directory as opposed to overlaoding th ePi with too many 2.4 MB photos in storage?

--- PiCamera/periodic_capture.py
+++ PiCamera/periodic_capture.py
@@ -17,32 +17,31 @@ GPIO.output(pin,0)
 # camera and timing elements
 camera = PiCamera()     # initialize camera
 numHours = 2
-secondsPerHour = 20
+secondsPerHour = 5
 running = 1          # running = button input signal indicates system on
 count = 0;
-img = 'image_'
+img = 'image_0'
 
 # display brief preview
 camera.start_preview()  # display camera preview on monitor
 sleep(5)
 camera.stop_preview()
 
-def db_upload(image):
-  upload_name = "/" + image;
+def db_upload(image, num):
+  upload_name = '/' + img + str(num) + '.jpg'
   file1 = open(image).read()
   db = dropbox.Dropbox('ippz4jAbhKAAAAAAAAAACSgGqnfO0L2JvjAb-YJ6l7KWZqo3uGLsjSU6d6afDKse')
   db.files_upload(file1,upload_name)
 
 # periodically capture pictures and upload to dropbox
 while running:
-  current_img = img + str(count)
+  current_img = img + '.jpg'
   count+=1
-  current_img = current_img + '.jpg'
   GPIO.output(pin,1) # turn on the LED
   sleep(0.2)
   camera.capture(current_img)
   sleep(0.2)
   GPIO.output(pin,0) # turn off the LED
-  db_upload(current_img)
+  db_upload(current_img, count)
   sleep(numHours*secondsPerHour)               
 
