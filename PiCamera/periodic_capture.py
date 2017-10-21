eb476277cf518e5501c1c2f729a8fae57c6fd601
dm20 <demcgrath2@gmail.com>
2017-10-20 21:25:05 -0700

    extensive update
    -added db_upload method
    -added LED drive
    -added delay 
    -added preview

--- PiCamera/periodic_capture.py
+++ PiCamera/periodic_capture.py
@@ -4,16 +4,41 @@
 
 from picamera import PiCamera
 from time import sleep
+import RPi.GPIO as GPIO
 
+# LED drive
+GPIO.setmode(GPIO.BCM);
+pin = 15;
+GPIO.setup(pin,GPIO.OUT);
+GPIO.setwarnings(False);
+
+# camera and timing elements
 camera = PiCamera()     # initialize camera
 numHours = 2
 secondsPerHour = 3600
-# running = button input signal indicates system on
-running = true
-while running
-  # image = take still shot 
-  camera.start_preview()  # display camera preview on monitor
+running = true          # running = button input signal indicates system on
+count = 0;
+img = 'image_'
+
+# display brief preview
+camera.start_preview()  # display camera preview on monitor
+sleep(5)
+camera.stop_preview()
+
+# periodically capture pictures and upload to dropbox
+while running:
+  current_img = id + str(count)
+  count += 1
+  GPIO.output(pin,1) # turn on the LED
+  sleep(0.2)
+  camera.capture(current_img)
+  sleep(0.2)
+  GPIO.output(pin,0) # turn off the LED
+  db_upload(current_img)
   sleep(numHours*secondsPerHour)               
-  camera.stop_preview() # end preview
-  # add image to storage, ensure wifi connection, if unavail. then store on Pi
   
+def db_upload(image):
+  upload_name = "/" + image;
+  file1 = open(image).read()
+  db = dropbox.Dropbox('ippz4jAbhKAAAAAAAAAACSgGqnfO0L2JvjAb-YJ6l7KWZqo3uGLsjSU6d6afDKse')
+  db.files_upload(file1,upload_name)
