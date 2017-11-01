721bacdf8b7d2a9461c680d75c91a7d3aa25b75e
dm20 <demcgrath2@gmail.com>
2017-10-31 18:23:09 -0700

    add comment to path variable and fix formatting

--- Dropbox/dropbox_interface.py
+++ Dropbox/dropbox_interface.py
@@ -1,6 +1,6 @@
 from picamera import PiCamera
-from time import sleep
 import RPi.GPIO as GPIO
+from time import sleep
 import dropbox
 
 class Uploader():
@@ -17,12 +17,12 @@ class Uploader():
   captureInterval = secondsPerHour/photosPerHour
   hoursPerWeek = 168   # 168 hours in a week
   numberOfPhotos = hoursPerWeek*photosPerHour
-  running = 1          # running = button input signal indicates system on
+  running = 1   # running = button input signal indicates system on
   count = 0
-  path = 'sample_capture_'
+  path = 'sample_capture_' # the uploaded path is in the format: "sample_capture_[count]_[month]_[day]_[year]_[hour]:[min]:[sec]"
 
-  def db_upload(self,image, num):
-    upload_filename = '/' + self.path + str(num) + '_' + time.strftime("%B_%d_%Y_%X") + '.jpg' # filename with date and photo number
+  def db_upload(self,image,num):
+    upload_filename = '/' + self.path + str(num) + '_' + time.strftime("%B_%d_%Y_%X") + '.jpg'
     fileToUpload = open(image).read()
     db = dropbox.Dropbox('') # initialize dropbox folder access (access key removed)
     db.files_upload(fileToUpload,upload_filename) # upload the file
@@ -36,9 +36,9 @@ class Uploader():
       GPIO.output(self.pin,1) # turn on the LED
       camera.capture(temp_path)     # capture photo while the light is on
       GPIO.output(self.pin,0) # turn off the LED      # turn off the light
-      self.db_upload(temp_path, self.count)
+      self.db_upload(temp_path,self.count)
       sleep(self.captureInterval - 0.5)  # delay between captures (GUI waits 0.5s per call)
-      camera.close()                                  # close the camera between captures in case it is needed elsewhere
+      camera.close()   # close the camera between captures in case it is needed elsewhere
       
   def stop(self):
       self.running = 0 # stop the run loop from capturing and uploading
