e1d1e779062af75adce7015d79061cb1b675021c
dm20 <demcgrath2@gmail.com>
2017-10-31 18:17:08 -0700

    organize variables and operations

--- Dropbox/dropbox_interface.py
+++ Dropbox/dropbox_interface.py
@@ -12,32 +12,33 @@ class Uploader():
   GPIO.output(pin,0)
 
   # class variables
-  numHours = 2
-  secondsPerHour = 5
+  photosPerHour = 1    # 1 photo/hour
+  secondsPerHour = 3600
+  captureInterval = secondsPerHour/photosPerHour
+  hoursPerWeek = 168   # 168 hours in a week
+  numberOfPhotos = hoursPerWeek*photosPerHour
   running = 1          # running = button input signal indicates system on
   count = 0
   path = 'sample_capture_'
 
   def db_upload(self,image, num):
-    upload_name = '/' + self.path + str(num) + '_' + time.strftime("%B_%d_%Y_%X") + '.jpg' # filename with date and photo number
-    file1 = open(image).read()
+    upload_filename = '/' + self.path + str(num) + '_' + time.strftime("%B_%d_%Y_%X") + '.jpg' # filename with date and photo number
+    fileToUpload = open(image).read()
     db = dropbox.Dropbox('') # initialize dropbox folder access (access key removed)
-    db.files_upload(file1,upload_name) # upload the file
+    db.files_upload(fileToUpload,upload_filename) # upload the file
 
   # periodically capture pictures and upload to dropbox
   def run(self):
-    if (self.running):
+    if (self.running & count < numberOfPhotos):
       camera = PiCamera() # open the camera
-      current_path = self.path + '.jpg' 
+      temp_path = self.path + '.jpg' 
       self.count+=1   # increment the photo ID count
       GPIO.output(self.pin,1) # turn on the LED
-      sleep(0.2)
-      camera.capture(current_path)     # capture photo while the light is on
-      sleep(0.2)
+      camera.capture(temp_path)     # capture photo while the light is on
       GPIO.output(self.pin,0) # turn off the LED      # turn off the light
-      self.db_upload(current_path, self.count)
-      sleep(self.numHours*self.secondsPerHour - 0.5)  # delay between captures (accounting for half second wait between runs for stop button)
-      camera.close()                                  # close the camera between captures in case the function exits
+      self.db_upload(temp_path, self.count)
+      sleep(self.captureInterval - 0.5)  # delay between captures (GUI waits 0.5s per call)
+      camera.close()                                  # close the camera between captures in case it is needed elsewhere
       
   def stop(self):
       self.running = 0 # stop the run loop from capturing and uploading
