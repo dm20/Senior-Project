f22c6f25e1429877a516b595f48e721111ab6b8b
dm20 <demcgrath2@gmail.com>
2018-02-27 14:56:24 -0800

    update

--- PiCamera/interface.py
+++ PiCamera/interface.py
@@ -12,33 +12,26 @@ class Uploader():
   # initialize LED drive pins
   GPIO.setwarnings(False);
   GPIO.setmode(GPIO.BCM);
-  pin6 = 6;
-  pin13 = 13; # our switch requires the current of at least 3 GPIO pins
-  pin19 = 19;
   pin26 = 26;
-  GPIO.setup(pin6,GPIO.OUT);
-  GPIO.setup(pin13,GPIO.OUT); #Configure pins as outputs
-  GPIO.setup(pin19,GPIO.OUT);
   GPIO.setup(pin26,GPIO.OUT);
-  # ensure initial state of the light system is off
-  GPIO.output(pin6,0);
-  GPIO.output(pin13,0);
-  GPIO.output(pin19,0);
   GPIO.output(pin26,0);
   
   # class variables
-  photosPerHour = 6    # 1 photo/hour
+  photosPerHour = 1    # 1 photo/hour
   secondsPerHour = 3600
   captureInterval = secondsPerHour/photosPerHour
   numberOfDays = 1
-  hoursPerDay = 7
-  hoursPerTrial = numberOfDays*hoursPerDay   # 168 hours in a week
+  hoursPerDay = 24
+  hoursPerTrial = numberOfDays*hoursPerDay   
   numberOfPhotos = hoursPerTrial*photosPerHour
   running = 1   # running = button input signal indicates system on
   count = 0
   path = 'slime_capture' # the  path is in the format: "sample_capture_[count]_[month]_[day]_[year]_[hour]:[min]:[sec]"
-  save_path = r'/home/pi/Desktop/Slime Growth Captures/'
-  currentPath = '';
+  currentPath = ''
+
+  save_path = r'/home/pi/Desktop/Slime_Captures_' + time.strftime("%h_%d_%Y_%X")
+  if not os.path.exists(save_path):
+    os.makedirs(save_path)
 
   ##################################  
   # periodically capture pictures  #
@@ -50,37 +43,48 @@ class Uploader():
       self.count += 1   # increment the photo ID count
 
       # turn on light
-      GPIO.output(self.pin6,1);
-      GPIO.output(self.pin13,1);
-      GPIO.output(self.pin19,1);
       GPIO.output(self.pin26,1);
 
       # Let camera adjust to light
-      sleep(4)
+      #sleep(2)
 
       upload_filename = self.path + str(self.count) + '_' + time.strftime("%B_%d_%Y_%X") + '.jpg'
       self.currentPath = os.path.join(self.save_path, upload_filename)
       camera.capture(self.currentPath)
       file = open(self.currentPath)
-      #sleep(2)#testing purposes to avoid hourly delay
 
       # turn off light
-      GPIO.output(self.pin6,0);
-      GPIO.output(self.pin13,0);
-      GPIO.output(self.pin19,0);
       GPIO.output(self.pin26,0);
 
-      sleep(self.captureInterval - 4.1)  # delay between captures (subtract GUI and capture delay)
       camera.close()   # close the camera between captures in case it is needed elsewhere
-
+    return
   #############################  
   # Discontinue Photo Capture #
   #############################
   def stop(self):
       self.running = 0 # stop the run loop from capturing and uploading
+      self.count = 0
+      self.save_path = self.generateFolder()
 
   #####################################
   # Get the path of the current image #
   #####################################
   def getCurrentImagePath(self):
     return self.currentPath
+
+  ############################################################################
+  # Make a new directory and update the location of where to save files path #
+  ############################################################################
+  def generateFolder(self):
+    newpath = r'/home/pi/Desktop/Slime_Captures_' + time.strftime("%h_%d_%Y_%X")
+    if not os.path.exists(newpath):
+      os.makedirs(newpath)
+    return newpath
+  
+  #############################  
+  # Discontinue Photo Capture #
+  #############################
+  def stop(self):
+      self.running = 0 # stop the run loop from capturing and uploading
+      self.count = 0
+      self.save_path = self.generateFolder()
